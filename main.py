a264cae918bfb509a42bf411b721852ea4dac6c7
robesch <robert.scherrer@uzh.ch>
2018-12-11 17:19:09 +0100

    first try for a main that includes distribution

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 import PyQt5.QtWidgets
 from PyQt5.QtCore import QTimer
 from typing import *  # library for type hints
+from enum import Enum
 
 from logs import *
 from global_variables import *
@@ -14,18 +15,32 @@ from gui import Santa_GUI
 from statistics import Statistics
 
 
+class Process_State(Enum):
+    start = 0
+    collect = 1
+    produce = 2
+    distribute = 3
+    finished = 4
+
+
 world = World("config.ini")  # reads Config and generates Resources, Locations, Deers
 
-iter_ = 0
 
+
+iter_ = 0
+state_ = Process_State.start
 
 # region Main Function
 def main():  # one step of the main loop
     global iter_
-    if iter_ == 0:
+    global state_
+
+    if state_ == Process_State.start:
         for deer in world.deers:  # All deers leave Santa's house
             deer.move_to_collect(world.dx, world.santa_house, world.N, world.markers)
-    elif iter_ <= world.T:
+        state_ = Process_State.collect
+
+    elif  state_ == Process_State.collect:
         for deer in world.deers:
             deer.move_to_collect(world.dx, world.santa_house, world.N, world.markers)
             for location in world.locations:  # checks if the deer hit a natural resource
@@ -41,15 +56,34 @@ def main():  # one step of the main loop
                         if not already_marked:
                             world.markers.append(deer.start_marker(location, world.santa_house.center))  # add marker
                     break  # one deer can not collect multiple Resources at once
-    elif iter_ > world.T:
-        # start distribution
+
+        # criteria to end collection
+        if iter_ > world.T:
+            state_ = Process_State.produce
+            # fixme: must also end when resouces are collected
+
+    elif  state_ == Process_State.produce:
+        # produce toys
         world.produce_toys()
         world.calculate_distribution()
-        for second in range(1, world.T + 1):  # main loop
+        state_ = Process_State.distribute
+        
+    elif  state_ == Process_State.distribute:
+        # start distribution
+        world.T_dist += iter_  # add time spent to the budget we have
+        time_left = world.T_dist - iter_
+        time_to_go_home = max(deer.steps_to_destination(dx, world.dx, world.santa_house.center) for deer in world.deers)
+        mainlog.debug(f"Needs {time_to_go_home} seconds and still has {time_left} seconds.")
+        if time_left <= time_needed:
+            # go home before the kids wake up
+            for deer in world.deers:
+                deer.return_to_home(world.dx, world.santa_house)
+        else:
+            # continue distribution    
             for deer in world.deers:
                 deer.move_to_distribute(world.dx, world.santa_house, world.distribution_paths)
 
-            mainlog.debug(f"Time: {second} / Deers: {world.deers} / Paths: {world.distribution_paths}")
+            mainlog.debug(f"Time: {time_} / Deers: {world.deers} / Paths: {world.distribution_paths}")
 
             # finish early if the job is done
             if all(path.is_finished() for path in world.distribution_paths):
@@ -58,13 +92,12 @@ def main():  # one step of the main loop
                     # the deers are resting
                     mainlog.debug(
                         f"Distribution finished by {len(world.deers)} deers on {len(world.distribution_paths)} paths.")
-                    break
-        # todo: now walk
+                    state_ = Process_State.finished
+                    gui_updates.stop()
+
 
     iter_ += 1/world.animation_smoothness
     world.gui_time += 1/world.animation_smoothness
-    #  if distribution_is_done:
-    #       gui_updates.stop()
 
     for marker in world.markers:  # marker cleanup todo: unify marker cleanup a posteriori and in real time
         if marker.is_disabled():
@@ -78,13 +111,13 @@ def animation_next():  # todo: export to some other file ?
     """
     Updates the program logic and GUI
     """
-    mainlog.debug("Called animation_next")
+#    mainlog.debug("Called animation_next")
     main()  # next step of loop
     gui.update_world(world)  # update
     gui.repaint()            # GUI
 
     stats.update(iter_)  # update stats
-    mainlog.debug(f"Time: {iter_} seconds / Deers: {world.deers} / Resources: {world.resources} / Markers: {world.markers}")
+#    mainlog.debug(f"Time: {iter_} seconds / Deers: {world.deers} / Resources: {world.resources} / Markers: {world.markers}")
 
 # endregion
 
